d5a362106856d6e9b16a061db14a609e92d63179
NikitaSukhanov <nvsukhanov@mail.ru>
2019-03-13 00:36:23 +0300

    #4 Added menu to second screen
    - Added layout for menu
    - Added menu creating to the java class of the second activity

--- app/src/main/java/com/mithridat/nonoconverter/activities/ResultActivity.java
+++ app/src/main/java/com/mithridat/nonoconverter/activities/ResultActivity.java
@@ -2,9 +2,12 @@ package com.mithridat.nonoconverter.activities;
 
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.mithridat.nonoconverter.R;
@@ -17,6 +20,30 @@ public class ResultActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_result);
         findViewById(R.id.btnSaveThumb).setOnClickListener(this);
         findViewById(R.id.btnSaveNon).setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_result, menu);
+        setTitle(R.string.result_activity_name);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null)
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.miHome:
+                return true;
+            case android.R.id.home:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void createDialogDone() {
