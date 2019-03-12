76afcb52cde89326e71dce88965ed7cc06da4b8c
NikitaSukhanov <nvsukhanov@mail.ru>
2019-03-12 23:51:28 +0300

    #4 Created layout for second screen
    - Created layout for the second activity
    - Created java class for the second activity
    - Added second activity to the manifest file

---
+++ app/src/main/java/com/mithridat/nonoconverter/activities/ResultActivity.java
@@ -0,0 +1,15 @@
+package com.mithridat.nonoconverter.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mithridat.nonoconverter.R;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
