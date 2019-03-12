ecfecbc30aa61676d4ec291b6f004d55a3a41b52
PawelMelnik <melnikpavel98@gmail.com>
2019-03-12 20:43:18 +0300

    #3 Created layout for first screen
    - Created layout for the first activity
    - Created java class for the first activity
    - Added first activity to the manifest file

---
+++ app/src/main/java/com/mithridat/nonoconverter/activities/EditImageActivity.java
@@ -0,0 +1,17 @@
+package com.mithridat.nonoconverter.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mithridat.nonoconverter.R;
+
+
+public class EditImageActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_image);
+    }
+
+}
