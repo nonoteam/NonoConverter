94e7c674c73bd504327ad8b2c2af6e72c3be9900
ISurikov <ilya_curi@mail.ru>
2019-03-23 14:14:13 +0300

    #36 Refactored first screen code
    - Changed IDs in xml
    - Changed strings names
    - Added comments
    - Changed class member ordering

--- app/src/main/java/com/mithridat/nonoconverter/ui/editimage/EditImageActivity.java
+++ app/src/main/java/com/mithridat/nonoconverter/ui/editimage/EditImageActivity.java
@@ -12,47 +12,39 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 
 import com.mithridat.nonoconverter.R;
 import com.mithridat.nonoconverter.ui.result.ResultActivity;
 
 import java.util.concurrent.TimeUnit;
 
-
-public class EditImageActivity extends AppCompatActivity implements View.OnClickListener {
-
-    ProgressDialog progressDialog;
-    AsyncTaskTransition atTransition = null;
-
-    OnCancelListener pdCancelListener = new OnCancelListener() {
-        @Override
-        public void onCancel(DialogInterface dialogInterface) {
-            if (atTransition != null)
-                atTransition.cancel(true);
-        }
-    };
-
+/**
+ * Class for the editing image activity.
+ */
+public class EditImageActivity extends AppCompatActivity implements OnClickListener {
+    ProgressDialog _pdLoading;
+    AsyncTaskConvertImage _atConvert = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_image);
 
-        findViewById(R.id.btn_crop).setOnClickListener(this);
-        findViewById(R.id.btn_columns).setOnClickListener(this);
+        findViewById(R.id.button_crop).setOnClickListener(this);
+        findViewById(R.id.button_columns).setOnClickListener(this);
 
-        progressDialog = new ProgressDialog(this);
-        progressDialog.setMessage(
-            getResources().getString(R.string.pd_process_image)
-        );
+        _pdLoading = new ProgressDialog(this);
+        _pdLoading.setMessage(
+                getResources().getString(R.string.msg_process_image));
 
-        atTransition = (AsyncTaskTransition) getLastCustomNonConfigurationInstance();
-        if (atTransition != null && !atTransition.isCancelled() &&
-            atTransition.getStatus() == AsyncTask.Status.RUNNING) {
-
-            atTransition.link(this);
-            progressDialog.show();
-            progressDialog.setOnCancelListener(pdCancelListener);
+        _atConvert =
+                (AsyncTaskConvertImage) getLastCustomNonConfigurationInstance();
+        if (_atConvert != null && !_atConvert.isCancelled()
+                && _atConvert.getStatus() == AsyncTask.Status.RUNNING) {
+            _atConvert.link(this);
+            _pdLoading.show();
+            _pdLoading.setOnCancelListener(_diCancelListener);
         }
     }
 
@@ -60,7 +52,7 @@ public class EditImageActivity extends AppCompatActivity implements View.OnClick
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.menu_edit_image, menu);
-        setTitle(R.string.edit_image_activity_name);
+        setTitle(R.string.title_edit_image_activity);
         return true;
     }
 
@@ -68,11 +60,11 @@ public class EditImageActivity extends AppCompatActivity implements View.OnClick
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.miConvert:
-                progressDialog.show();
-                progressDialog.setOnCancelListener(pdCancelListener);
-                atTransition = new AsyncTaskTransition();
-                atTransition.link(this);
-                atTransition.execute();
+                _pdLoading.show();
+                _pdLoading.setOnCancelListener(_diCancelListener);
+                _atConvert = new AsyncTaskConvertImage();
+                _atConvert.link(this);
+                _atConvert.execute();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -82,9 +74,9 @@ public class EditImageActivity extends AppCompatActivity implements View.OnClick
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.btn_crop:
+            case R.id.button_crop:
                 break;
-            case R.id.btn_columns:
+            case R.id.button_columns:
                 createDialogColumns();
                 break;
             default:
@@ -100,6 +92,26 @@ public class EditImageActivity extends AppCompatActivity implements View.OnClick
                 R.anim.slide_out_right);
     }
 
+    @Override
+    public Object onRetainCustomNonConfigurationInstance() {
+        super.onRetainCustomNonConfigurationInstance();
+        if (_atConvert != null) _atConvert.unLink();
+        return _atConvert;
+    }
+
+    /**
+     * Provides cancellation of asynch task execution when the dialog is cancelled
+     */
+    OnCancelListener _diCancelListener = new OnCancelListener() {
+        @Override
+        public void onCancel(DialogInterface dialogInterface) {
+            if (_atConvert != null) _atConvert.cancel(true);
+        }
+    };
+
+    /**
+     * Create dialog for choosing number of columns or rows
+     */
     private void createDialogColumns() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         LayoutInflater inflater = LayoutInflater.from(this);
@@ -110,33 +122,19 @@ public class EditImageActivity extends AppCompatActivity implements View.OnClick
                         dialog.cancel();
                     }
                 })
-                .setPositiveButton("Change", new DialogInterface.OnClickListener() {
+                .setPositiveButton("Change",
+                        new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
                         //TODO: save columns and rows
                     }
-                });
-
-        builder.create().show();
+                })
+                .create()
+                .show();
     }
 
-    public Object onRetainCustomNonConfigurationInstance() {
-        if (atTransition != null)
-            atTransition.unLink();
-        return atTransition;
-    }
-
-    static class AsyncTaskTransition extends AsyncTask<Void, Void, Void> {
-
-        private EditImageActivity activity;
-
-        void link(EditImageActivity act) {
-            activity = act;
-        }
-
-        void unLink() {
-            activity = null;
-        }
+    static class AsyncTaskConvertImage extends AsyncTask<Void, Void, Void> {
+        private EditImageActivity _activity;
 
         @Override
         protected Void doInBackground(Void... params) {
@@ -148,20 +146,31 @@ public class EditImageActivity extends AppCompatActivity implements View.OnClick
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
             return null;
         }
 
         @Override
         protected void onPostExecute(Void result) {
             if (isCancelled()) return;
-            activity.progressDialog.dismiss();
-            Intent intent = new Intent(activity, ResultActivity.class);
-            activity.startActivity(intent);
-            activity.overridePendingTransition(
-                R.anim.slide_in_left,
-                R.anim.slide_out_left
-            );
+            _activity._pdLoading.dismiss();
+            Intent intent = new Intent(_activity, ResultActivity.class);
+            _activity.startActivity(intent);
+            _activity.overridePendingTransition(R.anim.slide_in_left,
+                    R.anim.slide_out_left);
+        }
+
+        /**
+         * Get link to EditImageActivity
+         */
+        void link(EditImageActivity act) {
+            _activity = act;
+        }
+
+        /**
+         * Reset link
+         */
+        void unLink() {
+            _activity = null;
         }
     }
 
