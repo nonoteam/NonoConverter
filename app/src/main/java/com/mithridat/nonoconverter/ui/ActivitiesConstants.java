c9c1b9192a7755cacab3b20850839990761ad17d
Mikhail Nakhatovich <mish_1998@mail.ru>
2019-03-25 12:32:33 +0300

    #42 Added ImagePicker activity
    - Created constants for requests activities
    - Added library for picking image
    - Added animation transitions to ImagePicker activity

---
+++ app/src/main/java/com/mithridat/nonoconverter/ui/ActivitiesConstants.java
@@ -0,0 +1,23 @@
+package com.mithridat.nonoconverter.ui;
+
+/**
+ * Class with constants for requests activities.
+ */
+public class ActivitiesConstants {
+
+    /**
+     * Request code for starting ImagePicker activity from StartActivity.
+     */
+    public static final int RC_PICK_IMAGE_START = 1;
+
+    /**
+     * Request code for starting ImagePicker activity from EditImageActivity.
+     */
+    public static final int RC_PICK_IMAGE_EDIT_IMAGE = 2;
+
+    /**
+     * Name of extra for putting path of image in intent.
+     */
+    public static final String EX_IMAGE_PATH = "ImagePath";
+
+}
