dccfa67e76b92dd100d48bc41571d65ca8b6042a
Smolskiy Maxim <mithridatus@mail.ru>
2019-04-16 22:37:38 +0300

    #98 Changed class Field

--- app/src/main/java/com/mithridat/nonoconverter/backend/nonogram/Field.java
+++ app/src/main/java/com/mithridat/nonoconverter/backend/nonogram/Field.java
@@ -1,14 +1,13 @@
 package com.mithridat.nonoconverter.backend.nonogram;
 
-import android.os.Parcel;
-import android.os.Parcelable;
+import android.graphics.Bitmap;
 
 import java.util.Arrays;
 
 /**
  * Field storage class
  */
-public class Field implements Parcelable {
+public class Field {
 
     /**
      * White color
@@ -31,6 +30,11 @@ public class Field implements Parcelable {
     public static final int ROW = 0, COL = 1;
 
     /**
+     * Constants for state type - indexes or colors
+     */
+    public static final int INDEX = 0, COLOR = 1;
+
+    /**
      * Field сolor matrix
      */
     private int[][] _field;
@@ -41,66 +45,65 @@ public class Field implements Parcelable {
     private int _rows, _cols;
 
     /**
-     * Constructor by the field sizes
-     *
-     * @param rows - number of field rows
-     * @param cols - number of field columns
+     * Array of colors that used in a puzzle. All cells of the field
+     * contain values of colors. But now it will contain
+     * indexes of these colors in the array _colors by default.
      */
-    public Field(int rows, int cols) {
-        _field = new int[rows][cols];
-        for (int i = 0; i < rows; i++) {
-            Arrays.fill(_field[i], EMPTY);
-        }
+    private int[] _colors;
+
+    /**
+     * Length of the array _colors
+     */
+    private int _colorsCount;
+
+    /**
+     * Identifier that indicates what is stored in the field's cells.
+     * Both colors and indexes of these colors can be stored in the cells
+     */
+    private int _state;
+
+    /**
+     * Constructor by black-and-white image
+     *
+     * @param bmp - black-and-white image
+     * @param rows - number of thumbnail rows
+     * @param cols - number of thumbnail columns
+     * @param p - fill parameter
+     */
+    public Field(Bitmap bmp, int rows, int cols, int p) {
+        _colors = new int[]{ BLACK, WHITE };
+        _colorsCount = _colors.length;
+        _state = INDEX;
         _rows = rows;
         _cols = cols;
+        _field = new int[rows][cols];
+        int height = bmp.getHeight(), width = bmp.getWidth();
+        int h = height / rows, w = width / cols;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                int sumColors = 0;
+                for (int x = j * w; x < (j + 1) * w; x++) {
+                    for (int y = i * h; y < (i + 1) * h; y++) {
+                        int color = bmp.getPixel(x, y);
+                        if (color == WHITE) {
+                            sumColors += 255;
+                        }
+                    }
+                }
+                int avgColor = sumColors / (w * h);
+                if (avgColor <= p) {
+                    _field[i][j] = Arrays.binarySearch(_colors, BLACK);
+                }
+            }
+        }
     }
 
     /**
-     * Constructor by the parcel
-     *
-     * @param source - parcel
+     * Private constructor without parameters
      */
-    private Field(Parcel source) {
-        _rows = source.readInt();
-        _cols = source.readInt();
-        _field = new int[_rows][_cols];
-        for (int i = 0; i < _rows; i++) {
-            for (int j = 0; j < _cols; j++) {
-                _field[i][j] = source.readInt();
-            }
-        }
+    private Field() {
+
     }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeInt(_rows);
-        dest.writeInt(_cols);
-        for (int i = 0; i < _rows; i++) {
-            for (int j = 0; j < _cols; j++) {
-                dest.writeInt(_field[i][j]);
-            }
-        }
-    }
-
-    public static final Parcelable.Creator<Field> CREATOR =
-            new Parcelable.Creator<Field>() {
-
-                @Override
-                public Field createFromParcel(Parcel source) {
-                    return new Field(source);
-                }
-
-                @Override
-                public Field[] newArray(int size) {
-                    return new Field[size];
-                }
-
-            };
 
     /**
      * Method for getting number of field rows
@@ -121,50 +124,105 @@ public class Field implements Parcelable {
     }
 
     /**
-     * Method for getting field cell color with coordinates (i,j)
+     * Method for getting length of _rows or _cols
      *
-     * @param i - field cell row number
-     * @param j - field cell column number
-     * @return field cell color with coordinates (i,j)
+     * @param type - ROW, if _rows
+     *               COL, if _cols
+     * @return length of _rows or _cols
      */
-    public int getColor(int i, int j) {
-        return _field[i][j];
+    public int getLength(int type) {
+        if (type == ROW) return getRows();
+        return getCols();
     }
 
     /**
-     * Method for setting field cell color with coordinates (i,j)
+     * Method for getting field cell color with coordinates (i,j) or (j, i)
      *
-     * @param i - field cell row number
-     * @param j - field cell column number
-     * @param col - color
+     * @param i - i
+     * @param j - j
+     * @param type - ROW, if (i, j)
+     *               COL, if (j, i)
+     * @return field cell color with coordinates (i,j) or (j, i)
      */
-    public void setColor(int i, int j, int col) {
-        _field[i][j] = col;
+    public int getColor(int i, int j, int type) {
+        if (type == ROW) return _field[i][j];
+        return _field[j][i];
     }
 
     /**
-     * Method for getting index of first cell with another color
-     * (current color - the color of cell with index = pos) in row or column
+     * Method for setting field cell color with coordinates (i,j) or (j, i)
      *
-     * @param ind - index of row or column
-     * @param pos - position, the search begins with the next cell
-     * @param dir - direction, +1 or -1
-     * @param type - ROW, if row
-     *               COL, if column
-     * @return index of first cell with another color in row or column
+     * @param i - i
+     * @param j - j
+     * @param color - color
+     * @param type - ROW, if (i, j)
+     *               COL, if (j, i)
      */
-    public int getAnotherColorIndex(int ind, int pos, int dir, int type) {
-        int i = pos + 1;
-        if (type == ROW) {
-            for (; i >= 0 && i < _cols; i += dir) {
-                if (_field[ind][i] != _field[ind][pos]) break;
-            }
-        } else {
-            for (; i >= 0 && i < _rows; i += dir) {
-                if (_field[i][ind] != _field[pos][ind]) break;
+    public void setColor(int i, int j, int color, int type) {
+        if (type == ROW) _field[i][j] = color;
+        _field[j][i] = color;
+    }
+
+    /**
+     * Method for getting count of colors
+     *
+     * @return count of colors
+     */
+    int getColors() {
+        return _colorsCount;
+    }
+
+    /**
+     * Method for getting thumbnail from Field
+     * (cell of the field converts in the pixel of the bitmap)
+     *
+     * @return thumbnail
+     */
+    Bitmap getBitmap() {
+        Field field = translateToColors();
+        Bitmap bmp = Bitmap.createBitmap(_cols,
+                _rows,
+                Bitmap.Config.ARGB_8888);
+        for (int i = 0; i < _rows; i++) {
+            for (int j = 0; j < _cols; j++) {
+                bmp.setPixel(j, i, field._field[j][i]);
             }
         }
-        return i;
+        return bmp;
+    }
+
+    /**
+     * Method for clearing field - all cells lose colors
+     */
+    void clear() {
+        for (int i = 0; i < _rows; i++) {
+            for (int j = 0; j < _cols; j++) {
+                _field[i][j] = EMPTY;
+            }
+        }
+    }
+
+    /**
+     * Method for getting new field with colors in cells
+     *
+     * @return new field
+     */
+    Field translateToColors() {
+        Field field = new Field();
+        field._colors = _colors;
+        field._colorsCount = _colorsCount;
+        field._state = COLOR;
+        field._rows = _rows;
+        field._cols = _cols;
+        field._field = _field;
+        if(_state == INDEX) {
+            for (int i = 0; i < field._rows; i++) {
+                for (int j = 0; j < field._cols; j++) {
+                    field._field[i][j] = _colors[_field[i][j]];
+                }
+            }
+        }
+        return  field;
     }
 
 }
