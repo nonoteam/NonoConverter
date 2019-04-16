2e4d7d22f9eea7bc8159d8dc8427c6d9fd80e858
Smolskiy Maxim <mithridatus@mail.ru>
2019-04-16 21:35:45 +0300

    #98 Changed class NonogramSolver

--- app/src/main/java/com/mithridat/nonoconverter/backend/solver/NonogramSolver.java
+++ app/src/main/java/com/mithridat/nonoconverter/backend/solver/NonogramSolver.java
@@ -13,24 +13,14 @@ import java.util.HashSet;
 public class NonogramSolver {
 
     /**
-     * Show what numbers are ready
-     */
-    boolean[][] _left, _top;
-
-    /**
      * Nonogram
      */
     Nonogram _nono;
 
     /**
-     * Field
-     */
-    Field _field;
-
-    /**
      * Async task of image converting
      */
-    AsyncTask<Void, Void, Field> _asyncTask;
+    AsyncTask<Void, Void, Nonogram> _asyncTask;
 
     /**
      * Show rows and columns in which there were changes during
@@ -39,17 +29,21 @@ public class NonogramSolver {
     HashSet<Integer> _rows, _cols;
 
     /**
-     * Constructor by the nonogram and async task of image converting
-     *
-     * @param nono - nonogram
-     * @param asyncTask - async task of image converting
+     * Set for storage numbers of states of state machine
+     * for algorithm that uses finite-state machine
      */
-    public NonogramSolver(
-            Nonogram nono,
-            AsyncTask<Void, Void, Field> asyncTask) {
-        _nono = nono;
-        _asyncTask = asyncTask;
-    }
+    HashSet<Integer> _states;
+
+    /**
+     * Finite-state machines for nonogram (for rows and columns)
+     */
+    StateMachine[] _rowsFSM, _colsFSM;
+
+    /**
+     * Constructor without parameters
+     */
+    public NonogramSolver()
+    {}
 
     /**
      * Method for setting nonogram
@@ -65,7 +59,7 @@ public class NonogramSolver {
      *
      * @param asyncTask - async task of image converting
      */
-    public void setAsyncTask(AsyncTask<Void, Void, Field> asyncTask) {
+    public void setAsyncTask(AsyncTask<Void, Void, Nonogram> asyncTask) {
         _asyncTask = asyncTask;
     }
 
@@ -78,10 +72,17 @@ public class NonogramSolver {
     public boolean solve() {
         boolean isChanged = true;
         init();
+        int rows = _nono.getLeftRowsLength(), cols = _nono.getTopColsLength();
+        for (int i = 0; i < rows; i++) {
+            applySimpleBoxes(i, Field.ROW);
+        }
+        for (int i = 0; i < cols; i++) {
+            applySimpleBoxes(i, Field.COL);
+        }
         while (!_asyncTask.isCancelled() && isChanged) {
-            isChanged = applyAlgorithms(_rows, Field.ROW);
+            isChanged = applyAlgorithm(_rows, Field.ROW);
             _rows.clear();
-            isChanged = applyAlgorithms(_cols, Field.COL) || isChanged;
+            isChanged = applyAlgorithm(_cols, Field.COL) || isChanged;
             _cols.clear();
         }
         return false;
@@ -92,29 +93,10 @@ public class NonogramSolver {
      */
     void init() {
         int rows = _nono.getLeftRowsLength(), cols = _nono.getTopColsLength();
-        _field = new Field(rows, cols);
-        _left = new boolean[rows][];
-        _top = new boolean[cols][];
         _rows = new HashSet<>();
         addIntervalHashSet(_rows, 0, rows);
         _cols = new HashSet<>();
         addIntervalHashSet(_cols, 0, cols);
-    }
-
-    /**
-     * Method for getting workspace in row or column:
-     * - each row of result matrix contains start position and
-     * next to last position
-     * of cell and number blocks
-     * - one row - one block
-     *
-     * @param ind - index of row or column
-     * @param type - ROW, if row
-     *               COL, if column
-     * @return workspace
-     */
-    int[][] getWorkspace(int ind, int type) {
-        return null;
     }
 
     /**
@@ -131,7 +113,7 @@ public class NonogramSolver {
     }
 
     /**
-     * Implementation of simple spaces algorithm
+     * Implementation of nonogram solving algorithm using finite-state machine
      *
      * @param ind - index of row or column
      * @param type - ROW, if row
@@ -139,90 +121,13 @@ public class NonogramSolver {
      * @return true, if any cell was filled
      *         false, otherwise
      */
-    boolean applySimpleSpaces(int ind, int type) {
+    boolean applyStateMachine(int ind, int type) {
         return false;
     }
 
     /**
-     * Implementation of forcing algorithm
-     *
-     * @param ind - index of row or column
-     * @param type - ROW, if row
-     *               COL, if column
-     * @return true, if any cell was filled
-     *         false, otherwise
-     */
-    boolean applyForcing(int ind, int type) {
-        return false;
-    }
-
-    /**
-     * Implementation of glue algorithm
-     *
-     * @param ind - index of row or column
-     * @param type - ROW, if row
-     *               COL, if column
-     * @return true, if any cell was filled
-     *         false, otherwise
-     */
-    boolean applyGlue(int ind, int type) {
-        return false;
-    }
-
-    /**
-     * Implementation of joining algorithm
-     *
-     * @param ind - index of row or column
-     * @param type - ROW, if row
-     *               COL, if column
-     * @return true, if any cell was filled
-     *         false, otherwise
-     */
-    boolean applyJoining(int ind, int type) {
-        return false;
-    }
-
-    /**
-     * Implementation of splitting algorithm
-     *
-     * @param ind - index of row or column
-     * @param type - ROW, if row
-     *               COL, if column
-     * @return true, if any cell was filled
-     *         false, otherwise
-     */
-    boolean applySplitting(int ind, int type) {
-        return false;
-    }
-
-    /**
-     * Implementation of punctuating algorithm
-     *
-     * @param ind - index of row or column
-     * @param type - ROW, if row
-     *               COL, if column
-     * @return true, if any cell was filled
-     *         false, otherwise
-     */
-    boolean applyPunctuating(int ind, int type) {
-        return false;
-    }
-
-    /**
-     * Implementation of dual position algorithm
-     *
-     * @param ind - index of row or column
-     * @param type - ROW, if row
-     *               COL, if column
-     * @return true, if any cell was filled
-     *         false, otherwise
-     */
-    boolean applyDualPosition(int ind, int type) {
-        return false;
-    }
-
-    /**
-     * For every row or column with index in set applies algorithms
+     * For every row or column with index in set applies algorithm
+     * using finite-state machine
      *
      * @param set - set of indexes
      * @param type - ROW, if row
@@ -230,20 +135,13 @@ public class NonogramSolver {
      * @return true, if any cell was filled
      *         false, otherwise
      */
-    private boolean applyAlgorithms(HashSet<Integer> set, int type) {
+    private boolean applyAlgorithm(HashSet<Integer> set, int type) {
         boolean isFilled = false, isChanged;
         for (int i : set) {
             if (_asyncTask.isCancelled()) break;
             isChanged = true;
             while (isChanged) {
-                isChanged = applySimpleBoxes(i, type);
-                isChanged = applySimpleSpaces(i, type) || isChanged;
-                isChanged = applyForcing(i, type) || isChanged;
-                isChanged = applyGlue(i, type) || isChanged;
-                isChanged = applyJoining(i, type) || isChanged;
-                isChanged = applySplitting(i, type) || isChanged;
-                isChanged = applyPunctuating(i, type) || isChanged;
-                isChanged = applyDualPosition(i, type) || isChanged;
+                isChanged = applyStateMachine(i, type);
                 isFilled = isChanged || isFilled;
             }
         }
@@ -261,22 +159,6 @@ public class NonogramSolver {
         for (int i = begin; i < end; i++) {
             set.add(i);
         }
-    }
-
-    /**
-     * Method for checking is all numbers are ready
-     *
-     * @param array - boolean array
-     * @return true, if all numbers are ready
-     *         false, otherwise
-     */
-    private boolean checkIsAllNumbersReady(boolean[][] array) {
-        for(boolean[] a : array) {
-            for(boolean b : a) {
-                if(!b) return false;
-            }
-        }
-        return true;
     }
 
 }
