ac4a04d241dc1577d7f950f13309dfdd398be65d
Smolskiy Maxim <mithridatus@mail.ru>
2019-04-19 16:31:02 +0300

    #98 Updated class NonogramSolver
    - Changed Field.COL by COL
    - Made getters package-private
    - Added TODO

--- app/src/main/java/com/mithridat/nonoconverter/backend/solver/NonogramSolver.java
+++ app/src/main/java/com/mithridat/nonoconverter/backend/solver/NonogramSolver.java
@@ -2,12 +2,12 @@ package com.mithridat.nonoconverter.backend.solver;
 
 import android.os.AsyncTask;
 
-import com.mithridat.nonoconverter.backend.nonogram.Field;
 import com.mithridat.nonoconverter.backend.nonogram.Nonogram;
 
 import java.util.HashSet;
 
 import static com.mithridat.nonoconverter.backend.nonogram.Field.ROW;
+import static com.mithridat.nonoconverter.backend.nonogram.Field.COL;
 
 /**
  * Nonogram solving class
@@ -47,49 +47,6 @@ public class NonogramSolver {
     public NonogramSolver() {}
 
     /**
-     * Method for getting nonogram
-     *
-     * @return nonogram
-     */
-    public Nonogram getNonogram() {
-        return _nono;
-    }
-
-    /**
-     * Method for getting _rows or _cols
-     *
-     * @param type - ROW, if _rows
-     *               COL, if _cols
-     * @return _rows or _cols
-     */
-    public HashSet<Integer> getSetWithChanges(int type) {
-        if (type == ROW) return _rows;
-        return _cols;
-    }
-
-    /**
-     * Method for getting _states
-     *
-     * @return _states
-     */
-    public HashSet<Integer> getStates() {
-        return _states;
-    }
-
-    /**
-     * Method for getting finite-state machines for nonogram
-     * (for rows or columns)
-     *
-     * @param type - ROW, if for rows
-     *               COL, if for columns
-     * @return finite-state machines for nonogram (for rows or columns)
-     */
-    public StateMachine[] getFSM(int type) {
-        if (type == ROW) return _rowsFSM;
-        return _colsFSM;
-    }
-
-    /**
      * Method for setting nonogram
      *
      * @param nono - nonogram
@@ -100,7 +57,7 @@ public class NonogramSolver {
         _rowsFSM = new StateMachine[rows];
         initFSM(_rowsFSM, ROW);
         _colsFSM = new StateMachine[cols];
-        initFSM(_colsFSM, Field.COL);
+        initFSM(_colsFSM, COL);
     }
 
     /**
@@ -121,21 +78,68 @@ public class NonogramSolver {
     public boolean solve() {
         init();
         applyAlgorithmSB(ROW);
-        applyAlgorithmSB(Field.COL);
+        applyAlgorithmSB(COL);
         while (!_asyncTask.isCancelled()) {
             if (!applyAlgorithmFSM(_rows, ROW)) return false;
             _rows.clear();
-            if (!applyAlgorithmFSM(_cols, Field.COL)) return false;
+            if (!applyAlgorithmFSM(_cols, COL)) return false;
             _cols.clear();
-            if(_rows.isEmpty()) break;
+            if (_rows.isEmpty()) break;
         }
+        if (_asyncTask.isCancelled()) return false;
         return _nono.checkCorrectness();
+    }
+
+    /**
+     * Method for getting nonogram
+     *
+     * @return nonogram
+     */
+    Nonogram getNonogram() {
+        return _nono;
+    }
+
+    /**
+     * Method for getting _rows or _cols
+     *
+     * @param type - ROW, if _rows
+     *               COL, if _cols
+     * @return _rows or _cols
+     */
+    HashSet<Integer> getSetWithChanges(int type) {
+        if (type == ROW) return _rows;
+        return _cols;
+    }
+
+    /**
+     * Method for getting _states
+     *
+     * @return _states
+     */
+    HashSet<Integer> getStates() {
+        return _states;
+    }
+
+    /**
+     * Method for getting finite-state machines for nonogram
+     * (for rows or columns)
+     *
+     * @param type - ROW, if for rows
+     *               COL, if for columns
+     * @return finite-state machines for nonogram (for rows or columns)
+     */
+    StateMachine[] getFSM(int type) {
+        if (type == ROW) return _rowsFSM;
+        return _colsFSM;
     }
 
     /**
      * Method for initialising class fields before solution start
      */
     void init() {
+        /*
+         *TODO: add `_nono.clearField();` when solver will be ready
+         */
         int rows = _nono.getLeftRowsLength(), cols = _nono.getTopColsLength();
         _rows = new HashSet<>();
         addIntervalHashSet(_rows, 0, rows);
