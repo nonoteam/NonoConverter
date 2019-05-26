6ca9be0e00142733365dcaf329321a0de9e14be0
NikitaSukhanov <nvsukhanov@mail.ru>
2019-05-27 00:16:34 +0300

    #184 Updated conversion dialog

--- app/src/main/java/com/mithridat/nonoconverter/ui/editimage/FragmentConvertDialog.java
+++ app/src/main/java/com/mithridat/nonoconverter/ui/editimage/FragmentConvertDialog.java
@@ -18,7 +18,7 @@ public class FragmentConvertDialog extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         return new AlertDialog.Builder(getActivity())
                 .setTitle(R.string.title_dialog_convert)
-                .setIcon(R.drawable.ic_error)
+                .setIcon(R.drawable.ic_dissatisfied)
                 .setMessage(R.string.msg_convert)
                 .setPositiveButton(R.string.action_ok, listenerOk)
                 .create();
